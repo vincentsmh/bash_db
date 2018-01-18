8d75d55a610ad133c791cbb05d7d17feb5d6c324
Vincent Huang <vincent.sm.huang@gmail.com>
2018-01-18 09:39:39 +0800

    Enhance performance
    Remove 'base64' on most data and change field seperater to '\,'

--- bash_db_tests.sh
+++ bash_db_tests.sh
@@ -8,15 +8,15 @@ source bash_unittest/bash_unittest
 # started with 'test_'
 function test_create_table()
 {
-  rm -rf ${db_file}
   local db_file="${HOME}/.bash_db/$(_b64e 'test_table')"
+  rm -rf ${db_file}
   create_table "test_table" "field1" "field2" "field3"
   assert_file_exist "${db_file}"
+  assert_file_empty "${db_file}"
 
-  local line="$(echo 'field1'|base64),$(echo 'field2'|base64)"
-  line="${line},$(echo 'field3'|base64)"
-  assert_eq "${line}" "$(head -n 1 ${db_file})"
-  assert_eq "0,6,6,6" "$(cat ${db_file} | sed -n 2p)"
+  assert_eq "field1\,field2\,field3" "$(head -n 1 ${db_file}.md)"
+  assert_eq "6\,6\,6" "$(cat ${db_file}.md | sed -n 2p)"
+  assert_eq "0" "$(cat ${db_file}.md | sed -n 3p)"
   rm -rf ${db_file}
 }
 
@@ -40,39 +40,41 @@ function test_exist_table()
 
 function test_insert_n_read()
 {
-  rm -rf ${test_db_table}
   local tb_name='test_table'
-  local test_db_table="${DB_DIR}/$(echo ${tb_name} | base64)"
+  local tb_file="$( _table_file "$tb_name" )"
+  rm -rf ${tb_file} ${tb_file}.md
   create_table "${tb_name}" "field1" "field2" "field3"
 
-  for i in $(seq 1 10); do
-    local v=$(printf "%-${i}s" "v")
+  for idx in $(seq 1 10); do
+    local v=$(printf "%-${idx}s" "v")
     v=$(echo ${v// /v}) # replace " " as "v"
     insert_record "${tb_name}" "${v}" "${v}" "${v}"
 
-    if [ $i -le 6 ]; then
-      assert_eq "${i},6,6,6" "$(cat ${test_db_table} | sed -n 2p)"
+    if [ $idx -le 6 ]; then
+      assert_eq "6\,6\,6" "$(cat ${tb_file}.md | sed -n 2p)"
     else
-      assert_eq "${i},${i},${i},${i}" "$(cat ${test_db_table} | sed -n 2p)"
+      assert_eq "${idx}\,${idx}\,${idx}" "$(cat ${tb_file}.md | sed -n 2p)"
     fi
 
-    local nth=$(($i + 2))
-    assert_eq "${i},$(_b64e ${v}),$(_b64e ${v}),$(_b64e ${v})" \
-      "$(cat ${test_db_table} | sed -n ${nth}p)"
+    assert_eq "$idx" "$(cat ${tb_file}.md | sed -n 3p)"
+    assert_eq "${idx}\,${v}\,${v}\,${v}" "$(cat ${tb_file} | sed -n ${idx}p)"
   done
 
   # Read all data
-  read_all ${tb_name} "tb_record"
+  read_all "${tb_name}" "tb_record"
+  local idx=0
   for i in $(seq 1 10); do
     v=$(printf "%-${i}s" "v")
     v=$(echo ${v// /v})
-    assert_eq "${tb_record[$i,'field1']}" "${v}"
-    assert_eq "${tb_record[$i,'field2']}" "${v}"
-    assert_eq "${tb_record[$i,'field3']}" "${v}"
+    idx=$(( $i - 1 ))
+    assert_eq "${tb_record[$idx,'ID']}" "${i}"
+    assert_eq "${tb_record[$idx,'field1']}" "${v}"
+    assert_eq "${tb_record[$idx,'field2']}" "${v}"
+    assert_eq "${tb_record[$idx,'field3']}" "${v}"
   done
 
   unset tb_record
-  rm -rf ${test_db_table}
+  rm -rf ${tb_file} ${tb_file}.md
 }
 
 function test_insert_nonexist_table()
@@ -86,7 +88,8 @@ function test_insert_nonexist_table()
 function test_print_table()
 {
   local tb_name="print_table"
-  rm -rf $(_table_file $tb_name)
+  local tb_file="$(_table_file $tb_name)"
+  rm -rf ${tb_file} ${tb_file}.md
   create_table "${tb_name}" "field1" "field2" "field3"
   for i in $(seq 1 10); do
     local v=$(printf "%-${i}s" "$i")
@@ -96,7 +99,7 @@ function test_print_table()
   echo -e
 
   print_table "print_table"
-  rm -rf $(_table_file $tb_name)
+  rm -rf ${tb_file} ${tb_file}.md
 }
 
 function test_set_db_dir()
